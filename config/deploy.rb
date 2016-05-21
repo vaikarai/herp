914e736d667d6ae7e712bef5e0e25d9126bbe90d
Senthil <senthil@erptek.com>
2016-05-21 01:18:22 -0400

    changed production server to herpprd

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :repo_url, 'git@github.com:vaikarai/herp.git'
 # setup rvm.
 set :rbenv_type, :system
 set :rbenv_ruby, '2.1.2'
+set :rbenv_path, '/home/herpprdadm/.rbenv' 
     
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
